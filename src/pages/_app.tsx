048b9e6baf527890fe469382307ce6f19f7a533e
Yogesh070 <ythapa430@gmail.com>
2023-03-20 23:50:03 +0545

    withLayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,38 @@ import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
+import type { NextPage } from 'next'
+import Head from "next/head";
+
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode
+}
+
+type PagePropsWithSession = {
+  session?: Session | null,
+}
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { session, ...pageProps }
+}: {
+  Component: NextPageWithLayout,
+  pageProps: PagePropsWithSession
 }) => {
+  const getLayout = Component.getLayout ?? ((page) => page)
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Rails</title>
+        <meta name="description" content="A project management tool used to plan, track, and manage projects." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SessionProvider session={session}>
+        {
+          getLayout(<Component {...pageProps} />)
+        }
+      </SessionProvider>
+    </>
   );
 };
 
