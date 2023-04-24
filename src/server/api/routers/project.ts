b616fbd7e344de07e976f41f4b6fe138d8482480
Yogesh070 <ythapa430@gmail.com>
2023-04-24 15:46:19 +0545

    project router

---
+++ src/server/api/routers/project.ts
@@ -0,0 +1,97 @@
+import { ProjectType } from "@prisma/client";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+interface ProjectInfo {
+    defaultWorkflows: string[];
+    projectName: string;
+}
+
+const projectTypesList: Record<ProjectType, ProjectInfo> =
+{
+    KANBAN: {
+        defaultWorkflows: ["Backlog", "In Progress", "Done"],
+        projectName: "Kanban",
+    },
+    SCRUM: {
+        defaultWorkflows: ["Backlog", "To Do", "In Progress", "Review", "Done"],
+        projectName: "Scrum",
+    }
+}
+
+export const projectRouter = createTRPCRouter({
+    getAllProjects: protectedProcedure.query(({ ctx }) => {
+        return ctx.prisma.project.findMany(
+            {
+                include: {
+                    projectLead: {
+                        select: {
+                            name: true,
+                        },
+                    },
+                },
+            }
+        );
+    }),
+    getProjectById: protectedProcedure.input(z.object({ id: z.string() })).query(({ ctx, input }) => {
+        return ctx.prisma.project.findUnique({
+            where: {
+                id: input.id,
+            },
+        });
+    }
+    ),
+    createProject: protectedProcedure.input(z.object({ name: z.string(), projectType: z.nativeEnum(ProjectType), })).mutation(async ({ ctx, input }) => {
+        const defaultWorkflows = projectTypesList[input.projectType].defaultWorkflows;
+        return ctx.prisma.project.create({
+            data: {
+                name: input.name,
+                projectType: input.projectType,
+                workflows: {
+                    createMany: {
+                        data: defaultWorkflows.map((workflow, index) => ({
+                            title: workflow,
+                            index: index,
+                        })),
+                        skipDuplicates: true,
+                    },
+                },
+
+            },
+        });
+    }),
+    updateProject: protectedProcedure.input(z.object({ id: z.string(), name: z.string(), projectType: z.nativeEnum(ProjectType), })).mutation(({ ctx, input }) => {
+        return ctx.prisma.project.update({
+            where: {
+                id: input.id,
+            },
+            data: {
+                name: input.name,
+                projectType: input.projectType,
+            },
+        });
+    }),
+    deleteProject: protectedProcedure.input(z.object({ id: z.string() })).mutation(({ ctx, input }) => {
+        return ctx.prisma.project.delete({
+            where: {
+                id: input.id,
+            },
+        });
+    }),
+
+    assignUserToProject: protectedProcedure.input(z.object({ projectId: z.string(), userId: z.string() })).mutation(({ ctx, input }) => {
+        return ctx.prisma.project.update({
+            where: {
+                id: input.projectId,
+            },
+            data: {
+                members: {
+                    connect: {
+                        id: input.userId,
+                    },
+                },
+            },
+        });
+    }),
+}
+);
