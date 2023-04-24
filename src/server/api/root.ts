5f93a39e6a15a626b80e1c95ad5cfd6370c8dea8
Yogesh070 <ythapa430@gmail.com>
2023-04-24 17:31:58 +0545

    router update

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
+import { projectRouter } from "./routers/project";
+import { userRouter } from "./routers/user";
+import { workflowRouter } from "./routers/workflow";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +9,9 @@ import { exampleRouter } from "./routers/example";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  project: projectRouter,
+  user: userRouter,
+  workflow: workflowRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/workflow.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const workflowRouter = createTRPCRouter({
+    createWorkflow: protectedProcedure.input(z.object({ title: z.string(), projectId: z.string(), index: z.number() })).mutation(({ ctx, input }) => {
+        return ctx.prisma.workFlow.create({
+            data: {
+                title: input.title,
+                projectId: input.projectId,
+                index: input.index,
+            },
+        });
+    }),
+});
