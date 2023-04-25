8ae132552201bea71f6944953d6ad77b4f800165
Yogesh070 <ythapa430@gmail.com>
2023-04-25 20:16:23 +0545

    [WIP] landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 
-import { api } from "../utils/api";
-
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from Rails" });
-
   return (
     <>
       <Head>
@@ -13,7 +9,9 @@ const Home: NextPage = () => {
         <meta name="description" content="A project management tool used to plan, track, and manage projects." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>{hello.data?.greeting}</h1>
+      <div>
+        <p>Landing Page</p>
+      </div>
     </>
   );
 };
