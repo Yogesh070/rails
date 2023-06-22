4216467899d1ea987ac8a239ddf63c270c3580c2
Yogesh070 <ythapa430@gmail.com>
2023-06-22 18:13:35 +0545

    workspace api added

---
+++ src/server/api/routers/workspace.ts
@@ -0,0 +1,132 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
+
+const generateUniqueShortName = async (ctx: any, name: string) => {
+    const shortName = name.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const existing = await ctx.prisma.workspace.findFirst({
+        where: {
+            shortName,
+        },
+    });
+    if (!existing) {
+        return shortName;
+    }
+    return `${shortName}-${Math.floor(Math.random() * 10000)}`;
+};
+
+const getRandomColor = () => {
+    return '#' + ((Math.random() * 0xffffff) << 0).toString(16);
+};
+
+
+export const workspaceRouter = createTRPCRouter({
+    getWorkspaces: protectedProcedure.query(async ({ ctx }) => {
+        return ctx.prisma.workspace.findMany({
+            where: {
+                OR: [
+                    {
+                        createdById: ctx.session.user.id,
+                    },
+                    {
+                        members: {
+                            some: {
+                                id: ctx.session.user.id,
+                            },
+                        },
+                    },
+                ],
+            },
+        });
+    }),
+    getWorkspaceById: protectedProcedure.input(z.object({workspaceId:z.string()})).query(async ({ ctx, input }) => {
+        return ctx.prisma.workspace.findUnique({
+            where: {
+                id: input.workspaceId,
+            },
+            include: {
+                members: true,
+                projects: true,
+            },
+        });
+    }),
+    createWorkspace: protectedProcedure.input(z.object({ name: z.string(), description: z.string().nullable() })).mutation(async ({ ctx, input }) => {
+        const shortName = await generateUniqueShortName(ctx, input.name);
+        const color = getRandomColor();
+        return ctx.prisma.workspace.create({
+            data: {
+                name: input.name,
+                shortName,
+                createdById: ctx.session.user.id,
+                color,
+            },
+        });
+    }),
+    addMemberToWorkspace: protectedProcedure.input(z.object({ workspaceId: z.string(), userId: z.string() })).mutation(({ ctx, input }) => {
+        return ctx.prisma.workspace.update({
+            where: {
+                id: input.workspaceId,
+            },
+            data: {
+                members: {
+                    connect: {
+                        id: input.userId,
+                    },
+                },
+
+            },
+        });
+    }),
+    removeMemberFromWorkspace: protectedProcedure.input(z.object({ workspaceId: z.string(), userId: z.string() })).mutation(({ ctx, input }) => {
+        return ctx.prisma.workspace.update({
+            where: {
+                id: input.workspaceId,
+            },
+            data: {
+                members: {
+                    disconnect: {
+                        id: input.userId,
+                    },
+                },
+
+            },
+        });
+    }),
+    updateWorkspaceName: protectedProcedure.input(z.object({ id: z.string(), name: z.string() })).mutation(({ ctx, input }) => {
+        return ctx.prisma.workspace.update({
+            where: {
+                id: input.id,
+            },
+            data: {
+                name: input.name,
+            },
+        });
+    }),
+    deleteWorkspace: protectedProcedure.input(z.object({ id: z.string() })).mutation(async ({ ctx, input }) => {
+        try {
+            const workspaceToDelete = await ctx.prisma.workspace.findFirstOrThrow({
+                where: {
+                    id: input.id,
+                },
+            });
+            if (ctx.session.user.id === workspaceToDelete?.createdById) {
+                return ctx.prisma.workspace.delete({
+                    where: {
+                        id: input.id,
+                    },
+                });
+            } else {
+                throw new TRPCError({
+                    code: "UNAUTHORIZED",
+                    message: "You Dont have access to delete the Workspace",
+                });
+            }
+        } catch (err) {
+            throw new TRPCError({
+                code: "BAD_REQUEST",
+                message: "An unexpected error occurred",
+            });
+        }
+    }),
+});
