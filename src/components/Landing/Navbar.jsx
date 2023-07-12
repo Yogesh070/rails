04afd7a1f38422667882ffbc2d0e1186cbc932a3
Yogesh070 <ythapa430@gmail.com>
2023-07-12 19:48:33 +0545

    updated with next components

--- src/components/Landing/Navbar.jsx
+++ src/components/Landing/Navbar.jsx
@@ -1,19 +1,22 @@
 import { ArrowRightIcon } from "@heroicons/react/24/outline"
 import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
-return (
+  return (
     <div className="flex header">
-    <Image src="logo.svg" alt="logo"  className="logo" height={24} width={24}/>
-     <nav>
-      <ul className="listed-items">
-        <li><a href="#Home">Home</a></li>
-        <li><a href="#Features">Features</a></li>
-        <li><a href="#About">About Us</a></li>
-        <li><a href="#Footer">Contact Us</a></li>
-      </ul>
-     </nav>
-     <button className="btn-signin flex gap-1">Sign in <ArrowRightIcon height={20}/> </button>
+      <Image src="logo.svg" alt="logo" className="logo" height={24} width={24} />
+      <nav>
+        <ul className="listed-items">
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#Features">Features</a></li>
+          <li><a href="#About">About Us</a></li>
+          <li><a href="#Footer">Contact Us</a></li>
+        </ul>
+      </nav>
+      <Link href="auth/signin">
+        <button className="btn-signin flex gap-1">Sign in <ArrowRightIcon height={20} /> </button>
+      </Link>
     </div>
   )
 }
