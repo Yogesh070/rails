2cdebf07901f777e2001c4b9f1cbb94fedd18222
Yogesh070 <ythapa430@gmail.com>
2023-08-30 13:20:31 +0545

    fixed url issue

--- src/server/services/email.service.ts
+++ src/server/services/email.service.ts
@@ -20,7 +20,7 @@ const sendEmail = async (to: string, subject: string, text: string,html?:string)
 const sendInviteEmail = async (to: string, token: string,workspaceName:string): Promise<any> => {
   const subject = 'Invitation to join a workspace';
   // replace this url with the link to the invitation page of your front-end app
-  const invitationUrl = http://localhost:3000/invite?token=${token};
+  const invitationUrl = `http://localhost:3000/invite?token=${token}`;
   const text = `Dear user,
   You have been invited to join a workspace. To accept the invitation, click on this link: ${invitationUrl}
   If you did not request any invitations, then ignore this email.`;
@@ -335,7 +335,7 @@ const sendInviteEmail = async (to: string, token: string,workspaceName:string): 
 const sendResetPasswordEmail = async (to: string, token: string): Promise<any> => {
   const subject = 'Reset password';
   // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = http://link-to-app/reset-password?token=${token};
+  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
   const text = `Dear user,
   To reset your password, click on this link: ${resetPasswordUrl}
   If you did not request any password resets, then ignore this email.`;
@@ -345,7 +345,7 @@ const sendResetPasswordEmail = async (to: string, token: string): Promise<any> =
 const sendVerificationEmail = async (to: string, token: string): Promise<any> => {
   const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = http://link-to-app/verify-email?token=${token};
+  const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
   const text = `Dear user,
   To verify your email, click on this link: ${verificationEmailUrl}
   If you did not create an account, then ignore this email.`;
